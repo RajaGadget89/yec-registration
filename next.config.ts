7669ae80271d48794a9e34281bb6bb48e805e15f
RajaGadgets89 <rajagadgets89@gmail.com>
2025-08-06 01:31:50 +0700

    fixed: Superbase upload data

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
 
+// ดึงโดเมนจาก ENV หรือตั้งค่าตรง ๆ ก็ได้
+const supabaseDomain = process.env.NEXT_PUBLIC_SUPABASE_URL?.replace(/^https?:\/\//, "") || "wwwzhpyvogwypmqgvtjv.supabase.co";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [supabaseDomain],
+  },
   ...(isProd && {
     output: "standalone",
     assetPrefix: "/",
